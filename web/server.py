cb587832f4e9f5f74bba7c2a4f541e7f04b4e962
harneizel <genyievg@gmail.com>
2024-05-18 21:21:49 +0400

    current temperature

--- web/server.py
+++ web/server.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import asyncio
 from database import models, requests
+import datetime
 
 
 app = Flask(__name__)
 
 @app.route('/') # отправка текущей темпы на сайт
-def get_temp():
+def send_temp():
     curr_temp = asyncio.run(requests.get_temp())
     print(f'ТЕКУЩАЯ ТЕМПЕРАТУРА {curr_temp}')
 
     return render_template('index.html', temp=curr_temp)
 
+@app.route('/get-temperature')
+def get_temp():
+    temp = request.args.get('temperature')
+    date_time = datetime.datetime.now()
+    current_date = datetime.date.today()
+    current_time = date_time.strftime("%H:%M:%S")
+    await requests.add_temp(current_date, current_time, temp)
+    print(f"Дата: {current_date}, время: {current_time}, температура: {temp}")
