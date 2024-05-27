409674582e67ebb2343b4207760ae0fac3efa7ec
harneizel <genyievg@gmail.com>
2024-05-28 03:04:59 +0400

    отправка температуры датчиком на сервер

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ from threading import Thread
 if __name__ == '__main__':
     try:
         asyncio.run(on_startup_database()) #подключение к бд
-        thread1 = Thread(target=asyncio.run, args=(get_temp(),))
-        thread1.start() # запуск сбора температуры
-        app.run(debug=True) # запуск сайта
+        #thread1 = Thread(target=asyncio.run, args=(get_temp(),))
+        #thread1.start() # запуск сбора температуры
+        app.run(host='192.168.254.184',port=5000, debug=True) # запуск сайта
     except:
         print("Завершение работы")
--- web/server.py
+++ web/server.py
@@ -6,20 +6,24 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route('/') # отправка текущей темпы на сайт
+@app.route('/', methods=["GET"]) # отправка текущей темпы на сайт
 def send_temp():
-    curr_temp = asyncio.run(requests.get_temp())
+    #curr_temp = asyncio.run(requests.get_temp())
+    curr_temp = 20
     print(f'ТЕКУЩАЯ ТЕМПЕРАТУРА {curr_temp}')
 
     return render_template('index.html', temp=curr_temp)
 
-@app.route('/get-temperature')
+
+@app.route('/get-temperature', methods=['POST'])
 def get_temp():
-    temp = request.args.get('temperature')
-    vcc = request.args.get("vcc")
+    print(request.form)
+    temp = request.form.get('temp')[:-1]
+    vcc = request.form.get('vcc')
     date_time = datetime.datetime.now()
     current_date = datetime.date.today()
     current_time = date_time.strftime("%H:%M:%S")
-    await requests.add_temp(current_date, current_time, temp)
-    print(f"Дата: {current_date}, время: {current_time}, температура: {temp}, вольтаж: {vcc}В")
+    #await requests.add_temp(current_date, current_time, temp)
+    print(f"Дата: {current_date}, время: {current_time}, температура: {temp}, вольтаж: {vcc} В")
+    return "Data received successfully"
 #
